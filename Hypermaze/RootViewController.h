c7f1c0d829e657ca3f129f80107520f96debf12c
Jakub Gruszecki <Jakub@macbook-pro-jakub-gruszecki.local>
2011-07-15 18:49:36 +0200

    Initial Commit

---
+++ Hypermaze/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Hypermaze
+//
+//  Created by Jakub Gruszecki on 11-07-15.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
---
+++ Hypermaze/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Hypermaze
+//
+//  Created by Jakub Gruszecki on 11-07-15.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
